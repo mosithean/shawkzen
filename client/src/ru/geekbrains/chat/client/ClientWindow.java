1b151ee0bc8ba7ff021a3260b9a56ec80e03a09c
mosithean <mosithean@gmail.com>
2021-12-04 15:29:40 +0100

    Create ClientWindow based on Swing

---
+++ client/src/ru/geekbrains/chat/client/ClientWindow.java
@@ -0,0 +1,28 @@
+package ru.geekbrains.chat.client;
+
+import javax.swing.*;
+
+public class ClientWindow extends JFrame {
+
+    private static final String IP_ADDR = "127.0.0.1";
+    private static final int PORT = 8189;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
+    private ClientWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
+    }
+}
