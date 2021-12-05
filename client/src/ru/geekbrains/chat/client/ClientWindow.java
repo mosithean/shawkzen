1f0590edce5edb6ea7fc397bf25f1bfd09c5285f
mosithean <mosithean@gmail.com>
2021-12-05 06:50:32 +0100

    Make ClientWindow a TCP Connection Listener

--- client/src/ru/geekbrains/chat/client/ClientWindow.java
+++ client/src/ru/geekbrains/chat/client/ClientWindow.java
@@ -1,20 +1,47 @@
 package ru.geekbrains.chat.client;
 
-import javax.swing.*;
+import ru.geekbrains.chat.network.TcpConnection;
+import ru.geekbrains.chat.network.TcpConnectionListener;
 
-public class ClientWindow extends JFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class ClientWindow extends JFrame implements ActionListener, TcpConnectionListener {
 
     private static final String IP_ADDR = "127.0.0.1";
     private static final int PORT = 8189;
     private static final int WIDTH = 600;
     private static final int HEIGHT = 400;
 
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickname = new JTextField("mosithean");
+    private final JTextField fieldInput = new JTextField();
+
+    private TcpConnection connection;
+
     private ClientWindow() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(null);
         setAlwaysOnTop(true);
+
+        log.setEnabled(false);
+        log.setLineWrap(true);
+        fieldInput.addActionListener(this);
+
+        add(log, BorderLayout.CENTER);
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldNickname, BorderLayout.NORTH);
+
         setVisible(true);
+        try {
+            connection = new TcpConnection(this, IP_ADDR, PORT);
+        } catch (IOException e) {
+            printMessage("Connection exception: " + e);
+        }
     }
 
     public static void main(String[] args) {
@@ -22,6 +49,37 @@ public class ClientWindow extends JFrame {
             @Override
             public void run() {
                 new ClientWindow();
+            }
+        });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    @Override
+    public void onConnectionReady(TcpConnection tcpConnection) {
+    }
+
+    @Override
+    public void onReceiveString(TcpConnection tcpConnection, String value) {
+    }
+
+    @Override
+    public void onDisconnect(TcpConnection tcpConnection) {
+    }
+
+    @Override
+    public void onException(TcpConnection tcpConnection, Exception e) {
+        printMessage("Connection exception: " + e);
+    }
+
+    private synchronized void printMessage(String msg) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(msg + '\n');
+                log.setCaretPosition(log.getDocument().getLength());
             }
         });
     }
